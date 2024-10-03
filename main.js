baf402f6d760fc8ec4a156be4f472f0819a9ff8d
Luan Fagioni <101909254+DevLuanFagioni@users.noreply.github.com>
2024-10-02 22:11:56 -0300

    upload files

---
+++ main.js
@@ -0,0 +1,16 @@
+// const verificar = document.getElementById("verificar")
+// const resposta = document.getElementById("resposta")
+
+
+// verificar.addEventListener('click', ()=>{
+//   const palindromo = document.getElementById("palindromo").value.trim().toLowerCase()
+
+//   const invertido = palindromo.split("").reverse().join("")
+
+//   if (palindromo === invertido) {
+//     resposta.textContent = "Sim, é um palindromo"
+//   } else {
+//     resposta.textContent = "Não é um palindromo"
+//   }
+
+// })
